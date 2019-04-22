d8eaabb353f8bf8183b6f149438f4ec6069b0a3f
IBM_Kimura <ibm_kimura@naotokimura.local>
2019-04-22 20:13:31 +0900

    リファクタ。async/awaitを使用

--- index.js
+++ index.js
@@ -1,47 +1,41 @@
-function getWiki(params) {
-    const mecab = require('mecabaas-client');
-    const wiki = require("wikijs").default;
-    var wikipedia = wiki({ apiUrl: "http://ja.wikipedia.org/w/api.php" });
-    var noun = '';
-    var noun_array = [];
+const mecab = require('mecabaas-client');
+const async = require('async');
+const wiki = require('wikijs').default({
+    apiUrl : 'http://ja.wikipedia.org/w/api.php'
+});
 
+function getWiki(params) {
     return new Promise(function(resolve, reject) {
-        //mecab
-        mecab.parse(params.sentence)
-        .then(function(morphs){
-            morphs.map(function(morph) {
-                if (morph[2] === '固有名詞') {
-                    noun_array.push(morph[0]);
-                    //wiki
-                    if(noun_array.length==1){
-                        console.log(noun_array[0]);
-                        wikipedia.search(noun_array[0])
-                        .then(data => {
-                            wikipedia
-                            .page(data.results[0])
-                            .then(page => {return page.summary()})
-                            .then(summary => {
-                                console.log(summary)
-                                resolve({"noun":morph[0], "msg" : summary});
-                            })
-                        });
-                    }
-                }
+        let noun = [];
+        const readAll = async() => {
+            //文中の最初に現れた固有名詞をMeCabで抽出。
+            let morphs = await mecab.parse(params.sentence);
+            await morphs.map(function( morph ) {
+                if (morph[2] === '固有名詞') noun.push(morph[0]);
             });
-        },
-        function(error){
-            console.log(error);
-            resolve({"msg" : "null"});
+            if (noun.length == 0) {
+                console.log('no noun');
+                resolve({"noun":"null", "msg":"null"});
+                return;
+            }
+            console.log(noun[0]);
+            
+            //Wikipediaで固有名詞を検索し、最初にヒットしたページから情報を抽出。
+            let search_results = await wiki.search(noun[0]);
+            if (search_results.length == 0) {
+                console.log('no wikipedia content');
+                resolve({"noun":"null", "msg":"null"});
+                return;
+            }
+            //ページ全体の情報を取得。
+            let page = await wiki.page(search_results.results[0]);
+            //概要を取得。
+            let summary = await page.summary();
+            resolve({"noun":encodeURI(noun[0]), "msg":summary});
         }
-    )
-    .then(()=>{
-        if(noun_array.length == 0){
-            console.log('no noun');
-            resolve({"msg" : "null"});
-        };
-    })
+        readAll();
     });
 }
-
 module.exports.main = getWiki;
-
+var params = {sentence:"siri"};
+getWiki(params);
